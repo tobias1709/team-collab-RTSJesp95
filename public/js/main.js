4a88453743230afd6c4aa179f1327e175a442543
RTSJesp95 <325598@rts-365.dk>
2019-08-16 11:07:24 +0200

    Deleted some Files

--- public/js/main.js
+++
@@ -1,8 +0,0 @@
-const searchBar = document.querySelector('#search-bar')
-
-searchBar.addEventListener('submit', event=>{
-    event.preventDefault();
-
-    const searchText = searchBar.querySelector('#search-bar-input').value;
-    document.location = `http://localhost:3000/search/${searchText}`;
-})
