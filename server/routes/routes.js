9b3b66a2afafbc9f8661527ceed68e705c8ad6fe
RTSJesp95 <325598@rts-365.dk>
2019-08-19 10:41:38 +0200

    Created Template Routes and added new files

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,5 +1,105 @@
 const mysql = require('../config/mysql');
 
 module.exports = (app) => {
+    async function getBrands() {
+        let db = await mysql.connect();
+        let [brands] = await db.execute(`
+           SELECT brand_id, brand_title, brand_image 
+           FROM brands`);
+        db.end();
+        return brands;
+    }
 
+    app.get('/', async (req, res, next)=>{
+        let brands = await getBrands();
+        let db = await mysql.connect();
+        let cars = await db.execute(`
+        SELECT
+          cars_id
+        , cars_title
+        , cars_description
+        , cars_topspeed
+        , cars_price
+        , cars_image
+        , brand_id
+        , brand_title
+        , brand_image
+        FROM cars
+        INNER JOIN brands ON brand_id = cars_brand_fk`)
+        db.end();
+        res.render('home', {
+            title: 'The Car Page',
+            'brands': brands,
+            'cars': cars[0]
+        })
+    })
+    app.get('/contact', async (req, res, next)=>{
+        let brands = await getBrands();
+        // let db = await mysql.connect();
+        // let cars = await db.execute(`
+        // SELECT
+        //   cars_id
+        // , cars_title
+        // , cars_description
+        // , cars_topspeed
+        // , cars_price
+        // , cars_image
+        // , brand_id
+        // , brand_title
+        // , brand_image
+        // FROM cars
+        // INNER JOIN brands ON brand_id = cars_brand_fk`)
+        // db.end();
+        res.render('contact', {
+            title: 'The Car Page',
+            'brands': brands
+            // 'cars': cars[0]
+        })
+    })
+    app.get('/brands', async (req, res, next)=>{
+        let brands = await getBrands();
+        // let db = await mysql.connect();
+        // let cars = await db.execute(`
+        // SELECT
+        //   cars_id
+        // , cars_title
+        // , cars_description
+        // , cars_topspeed
+        // , cars_price
+        // , cars_image
+        // , brand_id
+        // , brand_title
+        // , brand_image
+        // FROM cars
+        // INNER JOIN brands ON brand_id = cars_brand_fk`)
+        // db.end();
+        res.render('brands', {
+            title: 'The Car Page',
+            'brands': brands
+            // 'cars': cars[0]
+        })
+    })
+    app.get('/cars', async (req, res, next)=>{
+        let brands = await getBrands();
+        // let db = await mysql.connect();
+        // let cars = await db.execute(`
+        // SELECT
+        //   cars_id
+        // , cars_title
+        // , cars_description
+        // , cars_topspeed
+        // , cars_price
+        // , cars_image
+        // , brand_id
+        // , brand_title
+        // , brand_image
+        // FROM cars
+        // INNER JOIN brands ON brand_id = cars_brand_fk`)
+        // db.end();
+        res.render('cars', {
+            title: 'The Car Page',
+            'brands': brands
+            // 'cars': cars[0]
+        })
+    })
 };
