160a1b1ae88cf53e55cb74711c343a4e37114162
Tobias Kolstrup <5884577@RTS-365.dk>
2019-08-22 11:06:53 +0200

    outcomment loadingscreen

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-	setTimeout(() => {
-		document.querySelector('#loadingscreen').style.marginTop="-100vh";
-		document.querySelector('#wrapper').style.display="block";
-	}, 2000);
+// document.addEventListener('DOMContentLoaded', () => {
+// 	setTimeout(() => {
+// 		document.querySelector('#loadingscreen').style.marginTop="-100vh";
+// 		document.querySelector('#wrapper').style.display="block";
+// 	}, 2000);
 	
-	console.log("test");
-});
+// 	console.log("test");
+// });
